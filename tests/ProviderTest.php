67817dcd30434d022e534637582d45a092992fb1
Rafael Menezes <rafaelmenezes86@gmail.com>
2016-11-15 15:07:41 -0200

    Playing with depends and providers together

--- tests/ProviderTest.php
+++ tests/ProviderTest.php
@@ -36,6 +36,37 @@ class ProviderTests extends \PHPUnit_Framework_TestCase
     {
         return new CsvFileIterator('tests/products.csv');
     }
+
+    public function testProducerFirst()
+    {
+        $this->assertTrue(true);
+        return 'first';
+    }
+
+    public function testProducerSecond()
+    {
+        $this->assertTrue(true);
+        return 'second';
+    }
+
+    public function provider()
+    {
+        return array(array('provider 1'), array('provider 1'));
+    }
+
+    /**
+     * @depends testProducerFirst
+     * @depends testProducerSecond
+     * @dataProvider provider
+     */
+    public function testConsumer()
+    {
+        $this->assertEquals(
+          array('provider 1', 'first', 'second'),
+          func_get_args()
+        );
+    }
+
 }
 
 ?>
